94cf29b6b782b008bb393c4c31af5313c2a4cfb6
afran18 <afran.vk18@gmail.com>
2024-08-03 15:11:16 +0530

    Day 20 Done

---
+++ day20/day20.js
@@ -0,0 +1,10 @@
+// Day 20: LocalStorage and SessionStorage
+/*
+// Activity 1: Understanding LocalStorage
+// Task 1: Write a script to save a string value to localStorage and retrieve it. Log the retrieved value.
+// Save a string value to localStorage
+localStorage.setItem('myKey', 'Hello, LocalStorage!');
+const retrievedValue = localStorage.getItem('myKey');
+console.log(retrievedValue);
+
+*/
